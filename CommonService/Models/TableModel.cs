53ec7bc5f9b32962c555305ac3e8b904a6ef68a1
郝光明 <haoguangming@higocon.com>
2023-06-26 13:57:12 +0800

    *提交文件

---
+++ CommonService/Models/TableModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonService.Models
+{
+    public class TableModel
+    {
+        public string tableName { get; set; }
+        public string tableDesc { get; set; }
+    }
+}
